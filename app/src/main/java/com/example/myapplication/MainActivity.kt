06e5aa10b2c8cacae742916211cc41601e2c5a33
Daydeuk <71961156+Daydeuk@users.noreply.github.com>
2020-10-10 16:03:19 +0900

    view visibility 해보기

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,28 +2,39 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
+import kotlin.concurrent.timer
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         setTimeButton()
     }
 
     private fun setTimeButton() {
         time_button.setOnClickListener{
-            showMessage(getCurrentTime())
+            time_button.visibility= View.INVISIBLE
+           //showMessage(getCurrentTime())
+        }
+        timer(period = 1000, initialDelay = 1000)
+        {
+            runOnUiThread {
+                time_button.visibility = View.VISIBLE
+            }
         }
     }
+
+
     private fun getCurrentTime():String{
         return Calendar.getInstance().getTime().toString()
     }
     private fun showMessage(message: String) {
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
+
 
 }
